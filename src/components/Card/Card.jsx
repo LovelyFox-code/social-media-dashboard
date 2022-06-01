2aad9257b56d0158f578821d6772c4d7da236eac
Alina <alinadakhno60@gmail.com>
2022-06-01 11:01:15 +0200

    Card edit to styled components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import s from './Card.module.css'
+import styled from 'styled-components'
 
 
-export default function Card(props) {
-    
-  return (
-      
-    <div className={s.card}>{props.children}</div>
-  )
-}
+const Card = styled.div`
+    width: 250px;
+    background: #252B42;
+    border-radius: 5px;
+    padding: 35px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-top: 4px solid #178FF5;
+`;
+export default Card;
